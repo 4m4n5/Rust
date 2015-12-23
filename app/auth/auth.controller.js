ef099ebb8b01bc8308708e7aa3be91d7697891b9
Aman Shrivastava <aman.srivastava999@gmail.com>
2015-12-23 15:33:24 +0530

    Added Controller for Authentication

---
+++ app/auth/auth.controller.js
@@ -0,0 +1,18 @@
+angular.module('RustApp')
+  .controller('AuthCtrl', function(Auth, $state){
+    var authCtrl = this;
+
+    // The user object to be used with ng-model directive in the form
+    authCtrl.user = {
+      email: '',
+      password: ''
+    };
+
+    authCtrl.login = function(){
+      Auth.$authWithPassword(authCtrl.user).then(function(auth){
+        $state.go('home');
+      }, function(error){
+        authCtrl.error = error;
+      });
+    };
+  });
