a4d862af490a650a71a065720d9ab0cd106108fd
Aman Shrivastava <aman.srivastava999@gmail.com>
2015-12-23 16:33:54 +0530

    Added contoller for profile

---
+++ app/users/profile.controller.js
@@ -0,0 +1,14 @@
+angular.module('RustApp')
+  .controller('ProfileCtrl', function($state, md5, auth, profile){
+    var profileCtrl = this;
+
+    profileCtrl.profile = profile;
+
+    // Here we're getting the current user's email from the auth data that was
+    // resolved from our router, hashing it and setting to emailHash on profile
+    profileCtrl.updateProfile = function(){
+      // Doubtful about auth.password.email??
+      profileCtrl.profile.emailHash = md5.createHash(auth.password.email);
+      profileCtrl.profile.$save();
+    };
+  });
