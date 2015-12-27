1c9b57ea2bf535b31fa29de7b7b989829e998d0b
Aman Shrivastava <aman.srivastava999@gmail.com>
2015-12-27 13:11:27 +0530

    Fixed bugs

--- app/channels/channels.controller.js
+++ app/channels/channels.controller.js
@@ -4,9 +4,9 @@ angular.module('RustApp')
     channelsCtrl.profile = profile;
     channelsCtrl.channels = channels;
     channelsCtrl.getDisplayName = Users.getDisplayName;
-    channels.getGravatar = Users.getGravatar;
+    channelsCtrl.getGravatar = Users.getGravatar;
 
-    channels.logout = function(){
+    channelsCtrl.logout = function(){
       Auth.$unauth();
       $state.go('home');
     };
